4ed5eecd14345783e9d440d0bdc55600e8ab17fc
Alvaro Pena Ruiz <alvaropenaruiz@gmail.com>
2023-11-26 14:52:30 +0100

    adding morgan for request logs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,16 @@
 import express from 'express'
 import { appConfig } from './config/app'
 import { indexRouter } from './routes'
+import morgan from 'morgan'
 
 const app = express()
 const { port } = appConfig
+const environment = process.env.ENVIRONMENT
 
 app.use(express.json())
+
+if(environment == "development") app.use(morgan('common'))
+
 app.use('/api', indexRouter)  
 
 app.listen(port, () => {
