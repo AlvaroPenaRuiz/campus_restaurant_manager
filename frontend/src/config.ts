aeb687b935c2c5695239447bc551336c572f19c5
Álvaro Pena Ruiz <alvaropenaruiz@gmail.com>
2026-02-02 18:01:26 +0000

    despliegue para vps aunque hay que arreglar cosas

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,8 +1,24 @@
+const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
+
+const protocol = isLocal ? "http" : window.location.protocol.replace(":", "");
+const baseRoute = isLocal ? "localhost:3000" : window.location.host;
+
+export const config = {
+  backend: {
+    protocol,
+    baseRoute,
+  },
+};
+
+
+/*
 const config = {
     backend: {
         protocol: "http",
         baseRoute: "localhost:3000"
     }
 }
+
+*/
 
 export default config
